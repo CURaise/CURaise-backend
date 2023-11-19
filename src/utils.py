6cafb36dd0687e46a8205115490b28030fc0ae4e
Daniel Wang MBP <zw669@cornell.edu>
2023-11-19 03:03:38 -0500

    structure

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,20 @@ import json
 
 
 def success_message(x, code=201):
+    """
+    The success message.
+    :param x: Message to be returned. In dict
+    :param code: code to be returned. Default 201
+    :return: (json.dumps(x), code)
+    """
     return json.dumps(x), code
 
 
 def failure_message(x, code=400):
+    """
+    The failure message.
+    :param x: Message to be returned. In dict
+    :param code: code to be returned. Default 400
+    :return: (json.dumps(x), code)
+    """
     return json.dumps(x), code
